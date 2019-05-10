9a123f962e67130ceac8a105dc9e104e87dfafdf
Agustin Salinas <chaucharian@gmail.com>
2019-05-10 20:54:44 -0300

    improvement missing

--- server/api.js
+++ server/api.js
@@ -3,15 +3,14 @@ const app = express();
 const port = 3000;
 let humedity = 0, temperature = 0;
 
-app.listen(port, '0.0.0.0',() => console.log(`Example app listening on port ${port}!`));
+app.listen(port, '0.0.0.0',() => console.log(`This is real weed API is now working ${port}!`));
 
 app.get('/set', (req, res) => {
     humedity = req.query.h;
     temperature = req.query.t;
 });
 
-app.get('/data', (req, res) => {
+app.get('/data', res => {
     res.setHeader('Content-Type', 'application/json');
-    //res.json({ response: { temperature: 1, humedity: 2 } });
     return res.status(200).json({ humedity, temperature });
 });
