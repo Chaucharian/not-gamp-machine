3ec1e1452441e6fa3114baf6a0248d343085850c
Agustin Salinas <chaucharian@gmail.com>
2019-05-10 19:38:21 -0300

    relay handling temperature to turn it on and ui showing that

--- server/api.js
+++ server/api.js
@@ -5,15 +5,13 @@ let humedity = 0, temperature = 0;
 
 app.listen(port, '0.0.0.0',() => console.log(`Example app listening on port ${port}!`));
 
-//app.get('/', (req, res) =>   res.sendFile(path.join(__dirname+'/index.html')) );
-
 app.get('/set', (req, res) => {
     humedity = req.query.h;
     temperature = req.query.t;
-    console.log(humedity);
 });
 
 app.get('/data', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.json({ response: { temperature: 1, humedity: 2 } });
+    //res.json({ response: { temperature: 1, humedity: 2 } });
+    return res.status(200).json({ humedity, temperature });
 });
