a5edabe856089c91d9102c90e6308b5415dbb7b5
Agustin Salinas <chaucharian@gmail.com>
2019-05-11 00:36:08 -0300

    trying another thing

--- server.js
+++ server.js
@@ -2,22 +2,14 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
 // the __dirname is the current directory from where the script is running
 app.use(express.static('dist'));
-app.use(allowCrossDomain); 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // send the user to index html page inspite of the url
 app.get('/', (req, res) => {
