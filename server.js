564901de383a52582d0d465d1789bc5fd7d14ded
Agustin Salinas <chaucharian@gmail.com>
2019-05-11 00:31:49 -0300

    avoiding CORS

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
 // the __dirname is the current directory from where the script is running
 app.use(express.static('dist'));
+app.use(allowCrossDomain); 
 
 // send the user to index html page inspite of the url
 app.get('/', (req, res) => {
@@ -12,3 +25,4 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, '0.0.0.0',() => console.log(`Serving UI at ${port}!`));
+
