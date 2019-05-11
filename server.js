5a1abfe13b2b7d196ba2c6a35879783dc24a8465
Agustin Salinas <chaucharian@gmail.com>
2019-05-11 00:43:10 -0300

    another try

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
+app.use(cors());
 app.use(express.static('dist'));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 // send the user to index html page inspite of the url
 app.get('/', (req, res) => {
