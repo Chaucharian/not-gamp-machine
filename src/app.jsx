f684296ea659bc2a303b37e703ff120d81fa2078
Agustin Salinas <chaucharian@gmail.com>
2020-05-22 15:22:47 -0300

    fetching conditions

--- src/app.jsx
+++ src/app.jsx
@@ -6,14 +6,14 @@ const App = () => {
     const [currentTime, updateTime] = useState(Date.now());
     const [{ humedity, temperature}, setConditions] = useState({ humedity: 0, temperature: 0});
 
-    // useEffect( () => {
-    //     const id = setInterval(() => updateTime(Date.now()), 1000);
+    useEffect( () => {
+        const id = setInterval(() => updateTime(Date.now()), 1000);
 
-    //     getConditions().then( ({temperature, humedity }) => setConditions({ temperature, humedity }) )
-    //     .catch(error => console.error(error));
+        getConditions().then( ({temperature, humedity }) => setConditions({ temperature, humedity }) )
+        .catch(error => console.error(error));
 
-    //     return () => clearInterval(id);
-    // }, [currentTime]);
+        return () => clearInterval(id);
+    }, [currentTime]);
 
     useEffect( () => {
         getChartData(1565408398127, 1568086798127).then( ({temperature, humedity }) => setConditions({ temperature, humedity }) )
