fcaa7c645b55a57dc107871901101f5082472293
davidovichmarcos <davidovich.marcos@gmail.com>
2020-06-04 22:15:22 -0300

    chart working bitcheesss

--- src/app.jsx
+++ src/app.jsx
@@ -3,11 +3,21 @@ import { getChartData, getConditions } from './api';
 import DisplayData from './displayData';
 import ChartData from './chart';
 import "@babel/polyfill";
+import { subDays, addHours, format } from 'date-fns';
 
 const App = () => {
     const [currentTime, updateTime] = useState(Date.now());
     const [{ humedity, temperature}, setConditions] = useState({ humedity: 0, temperature: 0});
-    let [ chartData, setChartData] = useState([chartData]);
+    let [ chartData, setChartData] = useState([]);
+
+    const normalizeResponse = ({ data: response }) => {
+        const chartData = [['Time', 'Temp', 'Humidity']];
+        response.map( data => {
+            data.temperature !== 0 && chartData.push([new Date(data.timestamp).getHours()+"", Number(data.temperature.split(".")[0]), Number(data.humedity.split(".")[0])]);
+        });
+        return chartData;
+    }
+
     useEffect( () => {
         const id = setInterval(() => updateTime(Date.now()), 1000);
 
@@ -18,25 +28,15 @@ const App = () => {
     }, [currentTime]);
 
     useEffect( () => {
-        async function fetchData() {
-            return await getChartData(0, 1591206699963).then( res => {
-                let chartData = []; 
-                res.data.forEach(value => {
-                    var arr = Object.keys(value).map(function(k) { return value[k] });
-                    chartData.push(arr);
-                });
-                setChartData(chartData);
-            })
-            .catch(error => console.error(error));
-        }
-        fetchData();          
+        const yesterday = new Date(subDays(new Date, 1)).getTime();
+        const today = new Date().getTime();
+        getChartData(yesterday, today).then( response => setChartData(normalizeResponse(response)) );
     }, []);
-    
     
     return (
         <>
             <DisplayData temperature={temperature} humedity={humedity}/>
-            <ChartData data={chartData}/>
+            { chartData.length > 0 && <ChartData data={chartData}/> }
         </>
     );
 }
--- src/chart.jsx
+++ src/chart.jsx
@@ -2,23 +2,14 @@ import { Chart } from "react-google-charts";
 import * as React from "react";
 
 
-const ChartData = (chartData) => {
-      let mock = [
-        ['Year', 'Temp', 'Humidity'],
-        ['2014', 1000, 400],
-        ['2015', 1170, 460],
-        ['2016', 660, 1120],
-        ['2017', 1030, 540],
-      ];
-console.log(chartData);
-
+const ChartData = ({ data }) => {
   return (
 <Chart
   width={'600px'}
   height={'300px'}
   chartType="ColumnChart"
   loader={<div>Loading Chart</div>}
-  data={mock}
+  data={data}
   options={{
     backgroundColor: ""
   }}
