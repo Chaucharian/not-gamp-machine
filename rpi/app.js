d890aca020613fef29c4e791d88b968d1af13420
mdavidovich <davidovich.marcos@gmail.com>
2021-07-28 23:00:00 +0100

    updating app.js on rpi

--- rpi/app.js
+++ rpi/app.js
@@ -2,51 +2,52 @@
 const { exec } = require('child_process');
 const  https = require('https');
 
+exec('gpio mode 0 out');
+
 function transformData(data, callback) {
- const rawData = data.split('\n');
- const temperature = rawData[0];
- const humedity = rawData[1];
- callback({ temperature, humedity });
+	const rawData = data.split('\n');
+	const temperature = rawData[0];
+	const humidity = rawData[1];
+	callback({ temperature, humidity });
 }
 
-function readSensor(callback) {	
-exec('./HTU21D_test', {cwd: '/home/pi/Documents/Projects/rpi-examples/HTU21D/c'}, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return -1;
-  }
-  callback(stdout);
-});
-}
-
-function changeState(data) {
- const { temperature, humedity } = data;
- if(temperature >= 28) {
-	console.log("fan on");
-	exec('gpio write 7 0');  
- } else if (temperature <= 24){
-   	console.log("fan off");
-	exec('gpio write 7 1');
- }
-}
 function sendData(data) {
-  const { temperature, humedity } = data;
-  https.get(`https://not-gamp-api.herokuapp.com/set?t=${temperature}&h=${humedity}`,res => {
+  const { temperature, humidity } = data;
+  https.get(`https://notgamp.marplacode.com/api/conditions?t=${temperature}&h=${humidity}`,res => {
     console.log('statusCode:', res.statusCode);
   }).on('error', (e) => {
     console.error(e);
   });
 }
 
+function readSensor(callback) {
+	exec('./HTU21D_test', {cwd: '/home/pi/Documents/Projects/rpi-examples/HTU21D/c'}, (error, stdout, stderr) => {
+		if (error) {
+			console.error(`exec error: ${error}`);
+			return -1;
+		}
+		callback(stdout);
+	});
+}
+
+function changeState(data) {
+	const { temperature, humidity } = data;
+	if(temperature >= 28) {
+ 		exec('gpio write 0 1');
+ 	} else if(temperature <= 24){
+		exec('gpio write 0 0');
+	}
+
+}
+
 setInterval( () => {
- readSensor( data => {
-   transformData(data, data => {
-      let { temperature, humedity } = data;
-      temperature = Number(temperature.split('C')[0]);
-      humedity = Number(humedity.split('%')[0]);
-      exec('gpio mode 7 out'); // set out GPIO to out
-      changeState({ temperature, humedity });
-      sendData(data);
-   });
- });
-}, 500);
+	readSensor( data => {
+		transformData(data, data => {
+			let { temperature, humidity } = data;
+			temperature = Number(temperature.split('C')[0]);
+			humidity = Number(humidity.split('%')[0]);
+			changeState({ temperature, humidity });
+			sendData(data);	
+		});		
+	});
+}, 3000);
